039092ba2c3bff7c90e2349d011e2d142155dca0
Gautam Manohar <gcmanohar001@gmail.com>
2019-06-12 22:21:10 -0400

    Sync media every hour

--- enable-services.sh
+++ enable-services.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 unset services
-declare -a services=( 'mpd.service' 'checkmail.timer' 'syncthing.service' 'automount.service' 'inkscapefigures.service' 'inkscapewatch.service' )
+declare -a services=( 'mpd.service' 'syncthing.service' 'automount.service' 'inkscapefigures.service' 'inkscapewatch.service' 'checkmail.timer' 'syncmedia.timer' )
 for service in $services; do
     for action in "enable" "start"; do
         systemctl --user $action $service

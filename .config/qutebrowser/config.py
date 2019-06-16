f372f09813d07aa65fb54d42a95446c6c8088818
Gautam Manohar <gcmanohar001@gmail.com>
2019-06-15 21:31:10 -0400

    Fix store link

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -182,7 +182,7 @@ c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}',
 }
 
 # Org Capture
-config.bind('cl', "open javascript:location.href='org-protocol://store-link:?url='+encodeURIComponent(location.href)'")
+config.bind('cl', "open javascript:location.href='org-protocol://store-link?url='+ encodeURIComponent(location.href);")
 config.bind('cn', "open javascript:location.href='org-protocol://capture?template=n'")
 config.bind('cb', "open javascript:location.href='org-protocol://capture-html?template=p&url=' + encodeURIComponent(location.href) + '&title=' + encodeURIComponent(document.title || '[untitled page]') + '&body=' + encodeURIComponent(function () {var html = ''; if (typeof document.getSelection != 'undefined') {var sel = document.getSelection(); if (sel.rangeCount) {var container = document.createElement('div'); for (var i = 0, len = sel.rangeCount; i < len; ++i) {container.appendChild(sel.getRangeAt(i).cloneContents());} html = container.innerHTML;}} else if (typeof document.selection != 'undefined') {if (document.selection.type == 'Text') {html = document.selection.createRange().htmlText;}} var relToAbs = function (href) {var a = document.createElement('a'); a.href = href; var abs = a.protocol + '//' + a.host + a.pathname + a.search + a.hash; a.remove(); return abs;}; var elementTypes = [['a', 'href'], ['img', 'src']]; var div = document.createElement('div'); div.innerHTML = html; elementTypes.map(function(elementType) {var elements = div.getElementsByTagName(elementType[0]); for (var i = 0; i < elements.length; i++) {elements[i].setAttribute(elementType[1], relToAbs(elements[i].getAttribute(elementType[1])));}}); return div.innerHTML;}());")
 

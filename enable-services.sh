f8cbb0c9e3971491b083a81bbde0b1773bf1d247
Gautam Manohar <gcmanohar001@gmail.com>
2019-06-27 02:38:31 -0400

    Changed packages

--- enable-services.sh
+++ enable-services.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 unset services
-declare -a services=( 'mpd' 'syncthing' 'automount' 'inkscapefigures' 'inkscapewatch' 'checkmail.timer' 'syncmedia.timer' )
+declare -a services=( 'mpd' 'syncthing' 'automount' 'inkscapefigures' 'inkscapewatch' 'checkmail.timer' 'syncmedia.timer' 'bat-shutdown' )
 for service in $services; do
     for action in "enable" "start"; do
         systemctl --user $action $service

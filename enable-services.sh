15074466ce4efa9403da71c6ee76833df0ceb90a
Gautam Manohar <gcmanohar001@gmail.com>
2019-08-02 09:00:21 -0400

    Change batshutdown to timer

--- enable-services.sh
+++ enable-services.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 unset services
-declare -a services=( 'mpd' 'syncthing' 'automount' 'inkscapefigures' 'inkscapewatch' 'checkmail.timer' 'syncmedia.timer' 'bat-shutdown' )
+declare -a services=( 'mpd' 'syncthing' 'automount' 'inkscapefigures' 'inkscapewatch' 'checkmail.timer' 'syncmedia.timer' 'bat-shutdown.timer' )
 for service in $services; do
     for action in "enable" "start"; do
         systemctl --user $action $service
